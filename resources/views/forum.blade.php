2a3f7795f6544d3f192d25e6452e637130ea428f
Kesi Promnual <kesi.promnual@myzone.org>
2020-06-04 16:14:37 +0100

    Styling views a title

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -4,11 +4,24 @@
   @foreach($discussions as $d)
             <div class="panel panel-default">
                 <div class="panel-heading">
-                  <img src="{{$d->user->avatar}}" alt="">
+                  <img src="{{$d->user->avatar}}" alt="" width="40px" height="40px">
+                  <span>{{$d->user->name}}</span>
+                  <a href="{{route('discussion',['slug'=> $d->slug])}}" class="btn btn-default pull-right"> View</a>
                 </div>
 
                 <div class="panel-body">
-                    {{$d->content}}
+                  <h5 >
+                    {{ $d->title}}
+                  </h5>
+                  <h6 >
+                    {{ str_limit($d->content, 50)}}
+                  </h6>
+
+                </div>
+                <div class="panel-footer">
+                  <h6>
+                    {{ $d->replies->count()}} Replies
+                  </h6>
                 </div>
             </div>
   @endforeach
