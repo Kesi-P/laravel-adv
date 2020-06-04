d2ef5e8cf0751735234bbbebb34ec787ec12a199
Kesi Promnual <kesi.promnual@myzone.org>
2020-06-04 16:58:14 +0100

    The discussion page

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -2,12 +2,51 @@
 
 @section('content')
 
-            <div class="panel panel-default">
-                <div class="panel-heading">{{$discussion->title}}</div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <img src="{{$discussion->user->avatar}}" alt="" width="40px" height="40px">
+      <span>{{$discussion->user->name}}</span>
 
-                <div class="panel-body">
+    </div>
 
-                </div>
-            </div>
+    <div class="panel-body">
+      <h5 >
+        {{ $discussion->title}}
+      </h5>
+      <h6 >
+        {{ $discussion->content}}
+      </h6>
+
+    </div>
+    <div class="panel-footer">
+      <h6>
+        {{ $discussion->replies->count()}} Replies
+      </h6>
+    </div>
+    </div>
+
+      @foreach ($discussion->replies as $reply)
+      <div class="col-md-12">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <img src="{{$reply->user->avatar}}" alt="" width="40px" height="40px">
+            <span>{{$reply->user->name}},<b> {{$reply->created_at->diffForHumans()}}</b></span>
+
+          </div>
+
+          <div class="panel-body">
+
+            <h6 >
+              {{ $reply->content}}
+            </h6>
+
+          </div>
+        </div>
+      </div>
+
+      @endforeach
+
+
+
 
 @endsection
